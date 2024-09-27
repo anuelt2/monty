39f5112b1a5031f3761dec76e756ecc8c4e5fbb2
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-09-27 16:21:31 +0000

    Add _div function

--- op_math.c
+++ op_math.c
@@ -45,3 +45,31 @@ void sub(stack_t **stack, unsigned int line_number)
 	pop(stack, line_number);
 	(*stack)->n = result;
 }
+
+/**
+ * _div - Subtracts the top element from the second top element of a stack
+ * @stack: Pointer to top of the stack
+ * @line_number: Line number of command in file
+ *
+ * Return: Void
+ */
+
+void _div(stack_t **stack, unsigned int line_number)
+{
+	int quotient;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	quotient = (*stack)->next->n / (*stack)->n;
+	pop(stack, line_number);
+	(*stack)->n = quotient;
+}
