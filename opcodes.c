bfb064e11787c9bb7bbb5619d1cbd404a8dff445
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-09-27 17:57:04 +0000

    Modify call_opcodes function to handle pchar command

--- opcodes.c
+++ opcodes.c
@@ -16,7 +16,7 @@ void call_opcodes(stack_t **stack)
 		{"swap", swap}, {"add", add},
 		{"nop", nop}, {"sub", sub},
 		{"div", _div}, {"mul", mul},
-		{"mod", mod},
+		{"mod", mod}, {"pchar", pchar},
 		{NULL, NULL}
 	};
 	if (file_args->args[0] == NULL)
