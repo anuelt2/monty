ccfb0a776dde68ac0f6b117fbd48df646d4ea622
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-09-27 13:28:42 +0000

    Add pint function

--- print_stack.c
+++ print_stack.c
@@ -25,3 +25,22 @@ void pall(stack_t **stack, unsigned int line_number)
 		temp = temp->next;
 	}
 }
+
+/**
+ * pint - Prints all elements in a stack
+ * @stack: Pointer to top of stack
+ * @line_number: Line number of command in file
+ *
+ * Return: Void
+ */
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	fprintf(stdout, "%d\n", (*stack)->n);
+}
