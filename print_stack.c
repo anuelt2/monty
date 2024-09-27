7830d24bcd4c14f28e0cbb6f502b995ce99d624d
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-09-27 18:26:31 +0000

    Add pstr function

--- print_stack.c
+++ print_stack.c
@@ -68,3 +68,38 @@ void pchar(stack_t **stack, unsigned int line_number)
 
 	fprintf(stdout, "%c\n", (*stack)->n);
 }
+
+/**
+ * pstr - Prints the string starting at the top of the stack
+ * @stack: Pointer to top of stack
+ * @line_number: Line number of command in file
+ *
+ * Return: Void
+ */
+
+void pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	(void)line_number;
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stdout, "\n");
+		return;
+	}
+
+	temp = *stack;
+	while (temp != NULL)
+	{
+		if (temp->n <= 0 || temp->n > 127)
+		{
+			break;
+		}
+		else
+		{
+			fprintf(stdout, "%c", temp->n);
+			temp = temp->next;
+		}
+	}
+	fprintf(stdout, "\n");
+}
