d0734872dacb79f8ff71576e84c49fca3645a205
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-09-27 19:59:28 +0000

    Add rotr function

--- rotstack.c
+++ rotstack.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- * rotl - Swaps the top two elements of a stack
+ * rotl - Rotates the top element to the bottom of the stack
  * @stack: Pointer to top of the stack
  * @line_number: Line number of command in file
  *
@@ -29,4 +29,35 @@ void rotl(stack_t **stack, unsigned int line_number)
 	last->next = first;
 	first->prev = last;
 	first->next = NULL;
+}
+
+/**
+ * rotr - Rotates the bottom element to the top of the stack
+ * @stack: Pointer to top of the stack
+ * @line_number: Line number of command in file
+ *
+ * Return: Void
+ */
+
+void rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *first;
+	stack_t *last;
+
+	(void)line_number;
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	{
+		return;
+	}
+
+	first = last = *stack;
+	while (last->next != NULL)
+	{
+		last = last->next;
+	}
+	last->prev->next = NULL;
+	last->next = first;
+	first->prev = last;
+	last->prev = NULL;
+	*stack = last;
 }
