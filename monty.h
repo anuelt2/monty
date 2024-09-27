f33e95284eede1e85e772bf26078070b04d67465
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-09-27 13:09:11 +0000

    Add header file

---
+++ monty.h
@@ -0,0 +1,74 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**
+ * struct file - Arguments from file
+ * @stream: Data flow from file
+ * @buf: Particular line of data being read
+ * @line_number: Line number of particular line being read
+ * @args: Array of strings
+ *
+ * Description: Arguments from file
+ */
+
+typedef struct file
+{
+	FILE *stream;
+	char *buf;
+	unsigned int line_number;
+	char **args;
+} file;
+
+extern file *file_args;
+
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void check_args(int argc);
+char *readline(void);
+void init_file_args(void);
+void tokenize(void);
+int check_num(char *str);
+void call_opcodes(stack_t **stack);
+void free_args(void);
+void malloc_fail(void);
+
+#endif
